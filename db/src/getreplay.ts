a1cc4f0b5c6e7e25e63f7f448be125bedb671900
Jared Krinke <jared.krinke@gmail.com>
2020-01-03 08:50:03 -0800

    Remove unnecessary code

--- db/src/getreplay.ts
+++ db/src/getreplay.ts
@@ -1,4 +1,3 @@
-import * as LZString from "lz-string";
 import * as Firebase from "firebase-admin";
 import * as Slambda from "slambda";
 import * as sharedData from "shared-data";
@@ -6,8 +5,6 @@ import * as fbc from "fbc";
 
 const { createStringValidator, createNumberValidator } = Slambda;
 
-// Input validation
-// Note: If this gets obscured, this logic will probably need to change
 // TODO: Move into shared code lib
 const maxScore = 999999;
 
@@ -21,28 +18,11 @@ interface GetReplayResponse {
     replay: string;
 }
 
-const validateScore = Slambda.createValidator<GetReplayRequest>({
-    mode: createNumberValidator(1, 3),
-    initials: createStringValidator(/^[a-z]{3}$/),
-    score: createNumberValidator(0, maxScore),
-});
-
 // Database integration
 const root = Firebase
     .initializeApp({ credential: Firebase.credential.cert(fbc as Firebase.ServiceAccount) }, "getreplay")
     .firestore()
     .collection(sharedData.collection);
-
-// TODO: Share?
-interface ScoreDocument {
-    // Document name is the seed
-    mode: number;
-    host: string;
-    initials: string;
-    score: number;
-    timestamp: Firebase.firestore.Timestamp;
-    replay: Buffer;
-}
 
 export const handler = Slambda.createHandler<GetReplayRequest, GetReplayResponse>({
     method: "GET",
