39405bb2cd5033312e4b4d3f1287b7dad935af38
Jared Krinke <jared.krinke@gmail.com>
2020-01-06 08:23:01 -0800

    Reorganize

--- db/src/api.ts
+++ db/src/api.ts
@@ -5,12 +5,13 @@ import Cors from "@koa/cors";
 import BodyParser from "koa-bodyparser";
 import * as Validize from "validize";
 import * as Contract from "fbg-db-contract";
-import * as LZString from "lz-string";
+import LZString from "lz-string";
 
-const base64Pattern = /^[A-Za-z0-9+/=]*$/;
+const router = new Router();
+router.prefix("/.netlify/functions/api");
+
+// Top scores
 const validateMode = Validize.createIntegerValidator(1, 3, true);
-const validateSeed =  Validize.createStringValidator(/^[0-9a-f]{32}$/);
-
 const validateTopScoresParameters = Validize.createValidator<Contract.TopScoresRequestParameters>({
     mode: validateMode,
 });
@@ -19,6 +20,24 @@ const validateTopScoresQuery = Validize.createValidator<Contract.TopScoresReques
     includeSeeds: Validize.createOptionalValidator(Validize.createBooleanValidator(true)),
 });
 
+router.get(
+    Contract.TopScoresRoute,
+    Validize.validate((context) => {
+        context.validated = {
+            parameters: validateTopScoresParameters(context.params),
+            query: validateTopScoresQuery(context.query),
+        };
+    }),
+    (context) => {
+        const parameters: Contract.TopScoresRequestParameters = context.validated.parameters;
+        const query: Contract.TopScoresRequestQuery = context.validated.query;
+        context.status = 200;
+        context.body = `Here are the top scores for mode ${parameters.mode} (include scores: ${query.includeSeeds === true})`;
+    });
+
+// Add score
+const base64Pattern = /^[A-Za-z0-9+/=]*$/;
+const validateSeed =  Validize.createStringValidator(/^[0-9a-f]{32}$/);
 const validateAddScoreParameters = Validize.createValidator<Contract.AddScoreRequestParameters>({
     mode: validateMode,
     seed: validateSeed,
@@ -38,43 +57,24 @@ const validateAddScoreBody = Validize.createValidator<Contract.AddScoreRequestBo
     },
 })
 
-const router = new Router();
-router.prefix("/.netlify/functions/api");
+router.post(
+    Contract.AddScoreRoute,
+    Validize.validate((context) => {
+        context.validated = {
+            parameters: validateAddScoreParameters(context.params),
+            body: validateAddScoreBody(JSON.parse(context.request.rawBody)),
+        };
+    }),
+    (context) => {
+        const parameters: Contract.AddScoreRequestParameters = context.validated.parameters;
+        const body: Contract.AddScoreRequestBody = context.validated.body;
 
-router
-    .get(
-        Contract.TopScoresRoute,
-        Validize.validate((context) => {
-            context.validated = {
-                parameters: validateTopScoresParameters(context.params),
-                query: validateTopScoresQuery(context.query),
-            };
-        }),
-        (context) => {
-            const parameters: Contract.TopScoresRequestParameters = context.validated.parameters;
-            const query: Contract.TopScoresRequestQuery = context.validated.query;
-            context.status = 200;
-            context.body = `Here are the top scores for mode ${parameters.mode} (include scores: ${query.includeSeeds === true})`;
-        })
+        context.status = 200;
+        context.body = "";
+        console.log(`New score for mode ${parameters.mode} by ${body.initials} (${body.host}): ${body.score}\n${parameters.seed}\n${body.replay}`);
+    });
 
-    .post(
-        Contract.AddScoreRoute,
-        Validize.validate((context) => {
-            context.validated = {
-                parameters: validateAddScoreParameters(context.params),
-                body: validateAddScoreBody(JSON.parse(context.request.rawBody)),
-            };
-        }),
-        (context) => {
-            const parameters: Contract.AddScoreRequestParameters = context.validated.parameters;
-            const body: Contract.AddScoreRequestBody = context.validated.body;
-
-            context.status = 200;
-            context.body = "";
-            console.log(`New score for mode ${parameters.mode} by ${body.initials} (${body.host}): ${body.score}\n${parameters.seed}\n${body.replay}`);
-        })
-;
-
+// Set up app and handler
 const app = new Koa();
 app.use(Cors());
 app.use(BodyParser({ enableTypes: [ "text" ] }));
