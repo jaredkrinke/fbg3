1a11a977850ae99621dcf9ebba3e62aaab620947
Jared Krinke <jared.krinke@gmail.com>
2020-01-08 08:23:40 -0800

    Refactor

--- html/main.tsx
+++ html/main.tsx
@@ -63,128 +63,140 @@ if (parameters.has("mode") && parameters.has("seed")) {
     }
 
     class Replay extends React.Component<{ seed: string }, ReplayState> {
+        private static updatePeriodMS = 1000 / 60;
+
+        // TODO: Enum for pieces to ensure this covers the set
+        private static colors = [
+            "red",
+            "cyan",
+            "yellow",
+            "lime",
+            "green",
+            "magenta",
+            "orange",
+        ];
+
+        private board: React.RefObject<HTMLCanvasElement> = React.createRef<HTMLCanvasElement>();
+        private next: React.RefObject<HTMLCanvasElement> = React.createRef<HTMLCanvasElement>();
+
+        private boardContext: CanvasRenderingContext2D;
+        private nextContext: CanvasRenderingContext2D;
+
+        private replay?: fbg.Replay;
+        private gameState?: fbg.GameState;
+        private done: boolean = false;
+        private updateHandle: number | null = null;
+
         constructor(props) {
             super(props);
             this.state = {
                 level: 0,
                 lines: 0,
                 score: 0,
+            };
+        }
+
+        private static drawPiece(context: CanvasRenderingContext2D, piece: number[][], pieceIndex: number, x0: number, y0: number): void {
+            context.fillStyle = Replay.colors[pieceIndex];
+            for (let i = 0; i < piece.length; i++) {
+                const offsets = piece[i];
+                const x = offsets[0];
+                const y = offsets[1];
+                context.fillRect(x0 + x, y0 + y, 1, 1);
             }
         }
 
-        async componentDidMount() {
-            // TODO: This is huge and ugly; refactor, simplify, support unhooking on unmount, etc.
+        private visible(): boolean {
+            return !!(this.board.current && this.next.current);
+        }
 
+        private draw = () => {
+            if (this.visible()) {
+                this.boardContext.fillStyle = "black";
+                this.boardContext.fillRect(0, 0, fbg.boardWidth, fbg.boardHeight);
+                this.nextContext.fillStyle = "black";
+                this.nextContext.fillRect(0, 0, 4, 4);
+    
+                if (this.gameState) {
+                    const board = this.gameState.board;
+                    for (let i = 0; i < board.length; i++) {
+                        const row = board[i];
+                        for (let j = 0; j < row.length; j++) {
+                            if (row[j] !== 0) {
+                                this.boardContext.fillStyle = Replay.colors[row[j] - 1];
+                                this.boardContext.fillRect(j, i, 1, 1);
+                            }
+                        }
+                    }
+                
+                    const piece = this.gameState.piece;
+                    if (piece) {
+                        Replay.drawPiece(this.boardContext, piece, this.gameState.pieceIndex, this.gameState.pieceColumn, this.gameState.pieceRow)
+                    }
+                
+                    // Next piece
+                    const pieceNext = this.gameState.pieceNext;
+                    if (pieceNext) {
+                        Replay.drawPiece(this.nextContext, pieceNext, this.gameState.pieceNextIndex, 0, 3);
+                    }
+                }
+            }
+        }
+
+        private update = () => {
+            if (this.visible()) {
+                if (!this.replay.advanceFrame()) {
+                    if (!this.done) {
+                        this.done = true;
+                        clearInterval(this.updateHandle);
+                    }
+                }
+            
+                this.gameState = this.replay.getState();
+                const { level, lines, score } = this.gameState;
+                this.setState({
+                    level,
+                    lines,
+                    score,
+                });
+                requestAnimationFrame(this.draw);
+            }
+        }
+
+        // TODO: Unhook on dismount?
+        async componentDidMount() {
             const response = await fetch(getScoreUrl(parseInt(parameters.get("mode")), this.props.seed));
             if (response.ok) {
                 const replayComponent = this;
                 const data = await response.json();
 
-                // Initialize canvas
-                const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-                const context = canvas.getContext("2d");
+                if (this.visible()) {
+                    // Initialize canvas
+                    this.boardContext = this.board.current.getContext("2d");
+                    this.nextContext = this.next.current.getContext("2d");
 
-                const canvasNext = document.getElementById("canvasNext") as HTMLCanvasElement;
-                const contextNext = canvasNext.getContext("2d");
+                    const scale = this.board.current.width / 10;
+                    this.boardContext.translate(0, this.board.current.height);
+                    this.boardContext.scale(scale, -scale);
 
-                const scale = canvas.width / 10;
-                context.translate(0, canvas.height);
-                context.scale(scale, -scale);
-
-                contextNext.translate(0, canvasNext.height);
-                contextNext.scale(scale, -scale);
-
-                // TODO: Enum for pieces to ensure this covers the set
-                const colors = [
-                    "red",
-                    "cyan",
-                    "yellow",
-                    "lime",
-                    "green",
-                    "magenta",
-                    "orange",
-                ];
-
-                function drawPiece(context: CanvasRenderingContext2D, piece: number[][], pieceIndex: number, x0: number, y0: number) {
-                    context.fillStyle = colors[pieceIndex];
-                    for (let i = 0; i < piece.length; i++) {
-                        const offsets = piece[i];
-                        const x = offsets[0];
-                        const y = offsets[1];
-                        context.fillRect(x0 + x, y0 + y, 1, 1);
-                    }
+                    this.nextContext.translate(0, this.next.current.height);
+                    this.nextContext.scale(scale, -scale);
+    
+                    // Initialize replay
+                    this.replay = new fbg.Replay(data.replay);
+                    this.replay.start();
+                    this.updateHandle = setInterval(this.update, Replay.updatePeriodMS);
                 }
-                
-                function render(state: fbg.GameState): void {
-                    context.fillStyle = "black";
-                    context.fillRect(0, 0, fbg.boardWidth, fbg.boardHeight);
-                
-                    const board = state.board;
-                    for (let i = 0; i < board.length; i++) {
-                        const row = board[i];
-                        for (let j = 0; j < row.length; j++) {
-                            if (row[j] !== 0) {
-                                context.fillStyle = colors[row[j] - 1];
-                                context.fillRect(j, i, 1, 1);
-                            }
-                        }
-                    }
-                
-                    const piece = state.piece;
-                    if (piece) {
-                        drawPiece(context, piece, state.pieceIndex, state.pieceColumn, state.pieceRow)
-                    }
-                
-                    // Next piece
-                    contextNext.fillStyle = "black";
-                    contextNext.fillRect(0, 0, 4, 4);
-                    
-                    const pieceNext = state.pieceNext;
-                    if (pieceNext) {
-                        drawPiece(contextNext, pieceNext, state.pieceNextIndex, 0, 3);
-                    }
-                }
-
-                const replay = new fbg.Replay(data.replay);
-                replay.start();
-                
-
-                let done = false;
-                let updateHandle: number | null = null;
-                function renderReplay() {
-                    render(replay.getState());
-                }
-
-                function update(): void {
-                    if (!replay.advanceFrame()) {
-                        if (!done) {
-                            done = true;
-                            clearInterval(updateHandle);
-                        }
-                    }
-                
-                    const { level, lines, score } = replay.getState();
-                    replayComponent.setState({
-                        level,
-                        lines,
-                        score,
-                    });
-                    requestAnimationFrame(renderReplay);
-                }
-                
-                
-                const updatePeriodMS = 1000 / 60;
-                updateHandle = setInterval(update, updatePeriodMS);
             }
         }
 
         render() {
             return <div>
                 <div className="horizontal">
-                    <canvas id="canvas" width={200} height={400} tabIndex={1}></canvas>
+                    <canvas ref={this.board} width={200} height={400} tabIndex={1}></canvas>
                     <div className="vertical">
                         <p>Next</p>
-                        <canvas id="canvasNext" width={80} height={80}></canvas>
+                        <canvas ref={this.next} width={80} height={80}></canvas>
                     </div>
                 </div>
                 <p>Level: {this.state.level}</p>
@@ -228,7 +240,7 @@ if (parameters.has("mode") && parameters.has("seed")) {
                 body = <tr><td colSpan={2}>Loading...</td></tr>;
             }
 
-            return <table id="table1">
+            return <table>
                 <tr><th colSpan={2}>{this.props.title}</th></tr>
                 {body}
             </table>;
@@ -236,7 +248,7 @@ if (parameters.has("mode") && parameters.has("seed")) {
     }
 
     function Leaderboard() {
-        return <div className="horizontal" id="table">
+        return <div className="horizontal">
             <TopScoreTable title="Endless" mode={fbg.GameMode.endless}></TopScoreTable>
             <TopScoreTable title="Countdown" mode={fbg.GameMode.countdown}></TopScoreTable>
             <TopScoreTable title="Cleanup" mode={fbg.GameMode.cleanup}></TopScoreTable>
